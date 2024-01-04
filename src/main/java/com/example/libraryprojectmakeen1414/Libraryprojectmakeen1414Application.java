07177935734ab42336d063c0d9d9861e125930e1
neda <saeid@gmail.com>
2024-01-04 11:55:41 +0330

    Initial commit

---
+++ src/main/java/com/example/libraryprojectmakeen1414/Libraryprojectmakeen1414Application.java
@@ -0,0 +1,13 @@
+package com.example.libraryprojectmakeen1414;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Libraryprojectmakeen1414Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Libraryprojectmakeen1414Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/libraryprojectmakeen1414/Libraryprojectmakeen1414ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libraryprojectmakeen1414;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Libraryprojectmakeen1414ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
